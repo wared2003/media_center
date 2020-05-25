c54b411fc71e61c94f070b33e650658fa6708e2d
wared2003 <edouardn809@gmail.com>
2020-05-25 19:56:34 +0200

    ajout de commantaire succin

--- livre.py
+++ livre.py
@@ -4,16 +4,18 @@ import csv
 # class  definissant chaque livre
 class Livre:
 
-
+# initialisation des attributs de livre
     def __init__(self, nom, note):
         self.nom = nom
         self.auteur = auteur
         self.resume = resime
         self.note = note
 
+    # methode temporaire servant au deboguage
     def hey(self):
         print(self.nom)
 
+    # methode permetant de sauvegardé l'objet courant en bin
     def save(self):
         with open(self.nom + '.data', 'wb') as fic:
             mon_pickler = pickle.Pickler(fic)
--- main.py
+++ main.py
@@ -2,18 +2,23 @@
 import pickle
 from livre import Livre
 
+
+#liste contenant tous les item
 all_items = []
 
+# ouverture du fichier contenant tous les items enregistré
 with open('items.txt', "r") as items:
 
+# recuperation des  items enregisté
     for ligne in items:
         livre_name = ligne.strip()
         livre_data_path = livre_name + ".data"
-        with open(livre_data_path, "rb") as fic:
+        with open(livre_data_path, "rb") as fic:  # recuperations des instances enregisté de Livrev en bin
             get_record = pickle.Unpickler(fic)
             livre = get_record.load()
             all_items.append(livre)
 
+# affichage des items
 for item in all_items:
     print(item.nom)
 
